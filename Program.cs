317e057a005990752de8ce271212d4b8b2d7a416
Renata Bianchine <renatabianchine_@hotmail.com>
2021-08-11 08:28:33 -0300

    add program

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pastaEstudo
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string casa = "moveis";
+            Console.WriteLine(casa);
+
+            int idade = 100 ^ 25;
+            Console.WriteLine(idade);
+
+            Console.WriteLine($"{idade}, {casa}");
+
+
+        }
+    }
+}
