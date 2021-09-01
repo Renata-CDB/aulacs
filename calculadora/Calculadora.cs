bfd4fe253bb934b34a4882d501c6abf98352c814
Renata Bianchine <renatabianchine_@hotmail.com>
2021-09-01 08:31:55 -0300

    add soma

--- calculadora/Calculadora.cs
+++ calculadora/Calculadora.cs
@@ -7,10 +7,16 @@ namespace calculadora
         static void Main(string[] args)
         {
             string numero = "", numero2  = "";
+            double soma; 
+
             Console.WriteLine("Digite um numero");
             numero = Console.ReadLine();
             Console.WriteLine("Digite outro numero");
             numero2 = Console.ReadLine();
+
+            soma = Double.Parse(numero) + Double.Parse(numero2);
+            Console.WriteLine($"soma: {soma}");
+
         }
     }
 }
