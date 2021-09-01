55a0424175ff2cc53ad109338de4c64a0a934058
Renata Bianchine <renatabianchine_@hotmail.com>
2021-09-01 08:14:54 -0300

    add variables

---
+++ calculadora/Calculadora.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Calculadora
+    {
+        static void Main(string[] args)
+        {
+            string numero = "", numero2  = "";
+            Console.WriteLine("Digite um numero");
+            numero = Console.ReadLine();
+            Console.WriteLine("Digite outro numero");
+            numero2 = Console.ReadLine();
+        }
+    }
+}
