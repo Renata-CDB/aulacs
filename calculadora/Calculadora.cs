ca70450892e4eac5cb4b099f2ddebb7010671460
Renata Bianchine <renatabianchine_@hotmail.com>
2021-09-08 08:32:57 -0300

    inclusão operador /

--- calculadora/Calculadora.cs
+++ calculadora/Calculadora.cs
@@ -7,7 +7,7 @@ namespace calculadora
         static void Main(string[] args)
         {
             string numero = "", numero2  = "";
-            double soma; 
+            double soma, subtracao, divisao;
 
             Console.WriteLine("Digite um numero");
             numero = Console.ReadLine();
@@ -21,6 +21,14 @@ namespace calculadora
                 case "+":
                     soma = Double.Parse(numero) + Double.Parse(numero2);
                     Console.WriteLine($"soma: {soma}");
+                    break;
+                case "-":
+                    subtracao = Double.Parse(numero) - Double.Parse(numero2);
+                    Console.WriteLine($"subtração: {subtracao}");
+                    break;
+                case "/":
+                    divisao = Double.Parse(numero) / Double.Parse(numero2);
+                    Console.WriteLine($"divisão: {divisao}");
                     break;
             }
 
